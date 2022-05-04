827060af9c3603fe8c56b1433cc6f02c443da9e6
Gareth Donaldson <gareth@studiomindset.co.uk>
2022-05-04 17:44:46 +0100

    first commit

---
+++ app/root.tsx
@@ -0,0 +1,41 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import type { MetaFunction } from "@remix-run/react";
+
+import styles from "./styles/build.css";
+import { Layout } from "~/components/Layout";
+
+export const meta: MetaFunction = () => {
+  return { title: "New Remix App" };
+};
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="bg-black text-white">
+        <Layout>
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </Layout>
+      </body>
+    </html>
+  );
+}
